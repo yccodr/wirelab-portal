f25fd9329126024a74e63c88ceee42eab72a16ed
yccodr <taiwanmichael429@gmail.com>
2024-09-11 10:45:11 +0800

    feat: update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,38 @@
 import { signOut } from "firebase/auth";
 import { Button } from "./ui/button";
-import { firebaseAuth } from "@/lib/firebase/auth";
+import { firebaseAuth, useUser } from "@/lib/firebase/auth";
+import { Skeleton } from "./ui/skeleton";
+import { cn } from "@/lib/utils";
+import { Link } from "@tanstack/react-router";
 
 function Header() {
+  const { isLoggedIn, isLoading, signInWithGoogle } = useUser();
+
   return (
-    <div className="sticky top-0 z-50 flex items-center justify-between bg-background p-4 w-screen">
+    <div className="sticky top-0 z-50 flex items-center justify-between bg-background lg:py-6 lg:px-12 sm:py-4 sm:px-8 py-4 px-6 w-screen">
       <div className="flex items-center gap-2">
-        <span className="text-2xl font-bold">Wirelab Orientation</span>
+        <Link to="/">
+          <span className="text-2xl font-bold">Wirelab Orientation</span>
+        </Link>
       </div>
 
       <div className="flex items-center gap-2">
         <Button
-          className="text-lg font-bold"
-          type="button"
-          variant="link"
-          onClick={() => signOut(firebaseAuth)}
+          className="relative"
+          variant="ghost"
+          onClick={isLoggedIn ? () => signOut(firebaseAuth) : signInWithGoogle}
         >
-          登出
+          <div
+            className={cn(
+              "flex items-center gap-2 absolute left-0 w-full px-3 invisible",
+              { visible: isLoading }
+            )}
+          >
+            <Skeleton className="w-full h-4" />
+          </div>
+          <span className={cn({ "opacity-0": isLoading })}>
+            {isLoggedIn ? "登出" : "登入"}
+          </span>
         </Button>
       </div>
     </div>
